10ab4422c13c6add51881625337f8477ef60dd7e
Guilherme mesquita rocha <guilherme.mesquita.rocha@gmail.com>
2022-02-02 22:19:53 -0300

    adicionando sidebar

--- src/Hero.js
+++ src/Hero.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { FaHome, FaUserAlt, FaUsers, FaUserGraduate, FaDoorOpen } from "react-icons/fa";
+import './Hero.css'
+const Hero = ({ handleLogout }) => {
+  return <div className='body-hero'>
+    <div className='navigation'>
+      <ul>
+        <li>
+          <a href='#'>
+            <span className='icon'><FaHome className='fa' /></span>
+            <span className='title'>Inicio</span>
+          </a>
+        </li>
+        <li>
+          <a href='#'>
+            <span className='icon'><FaUserAlt className='fa' /></span>
+            <span className='title'>Perfil</span>
+          </a>
+        </li>
+        <li>
+          <a href='#'>
+            <span className='icon'><FaUsers className='fa' /></span>
+            <span className='title'>Cadastrar turmas</span>
+          </a>
+        </li>
+        <li>
+          <a href='#'>
+            <span className='icon'><FaUserGraduate className='fa' /></span>
+            <span className='title'>Alunos</span>
+          </a>
+        </li>
+        <li className='logout'>
+          <a href=''  onClick={handleLogout}>
+            <span className='icon'><FaDoorOpen className='fa ' /></span>
+            <span className='title'>Sair</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    
+  </div>
 
-const Hero = ({handleLogout}) => {
-  return <div>
-    <button onClick={handleLogout}>Sair</button>
-  </div>;
+  
 }
 
 export default Hero;
